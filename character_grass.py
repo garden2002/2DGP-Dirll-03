b1a7afc3ef89fa81a02e315a6756dc3c2d284e72
garden2002 <ehxhfl1017@naver.com>
2024-09-24 11:49:52 +0900

    기존코드 삭제, 뼈대 완성(프린트 무한루프 구조 확인)

--- character_grass.py
+++ character_grass.py
@@ -6,57 +6,19 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 400
-y = 90
-temp = 0
-while (True):
-    while(temp % 2 == 0):
-        while(x < 790):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            x = x + 5
-            delay(0.01)
-        while(y < 600):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            y = y + 5
-            delay(0.01)
-        while(x > 0):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            x = x - 5
-            delay(0.01)
-        while(y > 90):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            y = y - 5
-            delay(0.01)
-        while(x < 400):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            x = x + 5
-            delay(0.01)
-        temp = temp + 1
-    while(temp % 2 == 1):
-        radius = 0
-        while(radius < 360):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            x = 400 + 400 * math.sin((540 - radius) / 360 * 2 * math.pi)
-            y = 390 + 300 * math.cos((540 - radius) / 360 * 2 * math.pi)
-            radius = radius + 2
-            delay(0.01)
-        temp = temp + 1
-    
-    
-    
+def run_circle():
+    print('circle')
+    pass
+
+def run_rectangle():
+    print('rectangle')
+    pass
 
 # fill here
+while True:
+    run_circle()
+    run_rectangle()
+    
+
 
 close_canvas()
